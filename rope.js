f4a05b68724022708f2074501e7295e62a056fd5
vedant-pro <72551024+vedant-pro@users.noreply.github.com>
2021-02-06 15:29:59 +0530

    Update rope.js

--- rope.js
+++ rope.js
@@ -1,7 +1,7 @@
 class Rope{
     constructor(Body1,Body2,xOffset){
         var options={
-            bodyA:Body1,bodyB:Body2,pointB:{x:xOffset,y:0}
+            bodyA:Body1,bodyB:Body2,pointB:{x:xOffset,y:0},length:200,stiffness:0.5
 
         }
         this.xOffset=xOffset
@@ -11,6 +11,7 @@ class Rope{
     display(){
         var pointA=this.rope.bodyA.position
         var pointB=this.rope.bodyB.position
+        stroke("white")
         line(pointA.x,pointA.y,pointB.x+this.xOffset,pointB.y)
     }
 }
